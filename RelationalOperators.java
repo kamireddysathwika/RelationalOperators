d06a1a1d9d96d15b9182aad3719ce49f674a5cfe
kamireddysathwika <kamireddysathwika0908@gmail.com>
2025-01-30 14:47:07 +0530

    Add files via upload

---
+++ RelationalOperators.java
@@ -0,0 +1,29 @@
+public class RelationalOperators{
+ public static void main(String[] args) {
+        // Declare and initialize two variables
+        int a = 10;
+        int b = 20;
+
+        // Demonstrate the use of relational operators
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+        // Checking if a is less than b
+        System.out.println("a < b: " + (a < b));  // true
+
+        // Checking if a is greater than b
+        System.out.println("a > b: " + (a > b));  // false
+
+        // Checking if a is less than or equal to b
+        System.out.println("a <= b: " + (a <= b));  // true
+
+        // Checking if a is greater than or equal to b
+        System.out.println("a >> b: " + (a >= b));  // false
+
+        // Checking if a is equal to b
+        System.out.println("a == b: " + (a == b));  // false
+
+        // Checking if a is not equal to b
+        System.out.println("a != b: " + (a != b));  // true
+    }
+}
